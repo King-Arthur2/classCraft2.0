1437d9ad5ff9fa8d47a5cc442269692f4e51dab7
asmartinez <asmartinez@mticonsultoria.com>
2024-12-12 10:21:09 -0600

    Configuración adicional

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/classCraft2.0/', // Este debe coincidir con el nombre del repositorio en tu URL de GitHub Pages
+});
